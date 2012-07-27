2e82cadac082c8b7f7d3c5ccbcd1c90f63f89932
bodiless <hadrian@kalski.pl>
2012-07-27 14:45:03 +0200

    first commit

---
+++ ext13.py
@@ -0,0 +1,9 @@
+from sys import argv
+
+script, first, second, third = argv
+
+
+print "the name of script: ", script
+print "the first variable: ", first
+print "the second variable: ", second
+print "the third variable: ", third
